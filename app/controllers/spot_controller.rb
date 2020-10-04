289add4d1c2a2dfcfca158915d5208e042a08bbb
kymx1983 <kymx1983@gmail.com>
2020-10-04 14:41:33 +0900

    spot/create作成

--- app/controllers/spot_controller.rb
+++ app/controllers/spot_controller.rb
@@ -9,4 +9,17 @@ class SpotController < ApplicationController
   def new
     @spot = Spot.new
   end
+
+  def create
+    @spot = Spot.new
+    @spot.title = params[:title]
+    @spot.content = params[:content]
+    @spot.spot_address = params[:spot_address]
+
+    if @spot.save
+      redirect_to('/spot/index')
+    else
+      render("spot/new")
+    end
+  end
 end
