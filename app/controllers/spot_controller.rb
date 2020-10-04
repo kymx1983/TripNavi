a855473785fb3f522b04dfcc6541d69d22f34c5e
kymx1983 <kymx1983@gmail.com>
2020-10-04 16:41:51 +0900

    spot/show作成

--- app/controllers/spot_controller.rb
+++ app/controllers/spot_controller.rb
@@ -4,6 +4,7 @@ class SpotController < ApplicationController
   end
 
   def show
+    @spot = Spot.find_by(id:params[:id])
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'spot/index'
-  get 'spot/show'
+  get "spot/show/:id" => "spot#show"
   get 'spot/new'
   post "spot/create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
